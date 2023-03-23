d358faab9af9f1f4ee6b86198076c3166639a118
Tiann Mob <whysdevsrk@gmail.com>
2023-03-23 02:23:54 +0000

    circleci: add script for building

---
+++ build-start.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+# Copyright (C) 2020-2022 Oktapra Amtono <oktapra.amtono@gmail.com>
+# Docker Kernel Build Script
+
+# Clone kernel source
+if [[ "$*" =~ "stable" ]]; then
+    git clone --depth=1 https://github.com/TianWalkzzMiku/kernel-sdm660.git -b deagle kernel
+    cd kernel || exit
+fi
+
+# Clone toolchain
+if [[ "$*" =~ "clang" ]]; then
+    git clone --depth=1 https://github.com/kdrag0n/proton-clang clang
+elif [[ "$*" =~ "gcc" ]]; then
+    git clone --depth=1 https://github.com/avinakefin/GCC-12-arm32 arm32
+    git clone --depth=1 https://github.com/avinakefin/GCC-12-aarch64 arm64
+fi
+
+# Clone anykernel3
+git clone --depth=1 https://github.com/TianWalkzzMiku/AK3-4.4.git -b whyred ak3-whyred
+git clone --depth=1 https://github.com/TianWalkzzMiku/AK3-4.4.git -b tulip ak3-tulip
+
+# Telegram setup
+push_message() {
+    curl -s -X POST \
+        https://api.telegram.org/bot"{$BOT_TOKEN}"/sendMessage \
+        -d chat_id="${CHAT_ID}" \
+        -d text="$1" \
+        -d "parse_mode=html" \
+        -d "disable_web_page_preview=true"
+}
+
+# Push message to telegram
+push_message "
+<b>======================================</b>
+<b>Start Building :</b> <code>#DEAGLE Reborn</code>
+<b>Linux Version :</b> <code>$(make kernelversion | cut -d " " -f5 | tr -d '\n')</code>
+<b>Source Branch :</b> <code>$(git rev-parse --abbrev-ref HEAD)</code>
+<b>======================================</b> "
